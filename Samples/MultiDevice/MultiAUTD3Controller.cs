c4e8596848dee1a62c6275303fbc35079a0a3526
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-03-30 09:59:52 +0900

    bump to v22.0.4

---
+++ Samples/MultiDevice/MultiAUTD3Controller.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Linq;
+using AUTD3Sharp;
+using UnityEngine;
+
+#if UNITY_2020_2_OR_NEWER
+#nullable enable
+#endif
+
+public class MultiAUTD3Controller : MonoBehaviour
+{
+    private Controller<AUTD3Sharp.Link.Simulator>? _autd = null;
+    public GameObject? Target = null;
+    private Vector3 _oldPosition;
+
+    void Awake()
+    {
+        var builder = new ControllerBuilder();
+        foreach (var obj in FindObjectsOfType<AUTD3Device>(false).OrderBy(obj => obj.ID))
+            builder.AddDevice(new AUTD3(obj.transform.position).WithRotation(obj.transform.rotation));
+
+        try
+        {
+            _autd = builder.Open(AUTD3Sharp.Link.Simulator.Builder(8080));
+        }
+        catch (Exception)
+        {
+            Debug.LogError("Before running this sample, install autd3-server (https://github.com/shinolab/autd3-server) and run simulator with unity option.");
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+#endif
+        }
+
+        _autd!.Send(new AUTD3Sharp.Modulation.Sine(150)); // 150 Hz
+
+        if (Target == null) return;
+        _autd!.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
+        _oldPosition = Target.transform.position;
+    }
+
+    private void Update()
+    {
+        if (Target == null || Target.transform.position == _oldPosition) return;
+        if (_autd == null) return;
+        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
+        _oldPosition = Target.transform.position;
+    }
+
+    private void OnApplicationQuit()
+    {
+        _autd?.Dispose();
+    }
+}
+
+#if UNITY_2020_2_OR_NEWER
+#nullable restore
+#endif
---
+++ Samples/Simple/SimpleAUTDController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using AUTD3Sharp;
+using AUTD3Sharp.Link;
+using AUTD3Sharp.NativeMethods;
+using UnityEngine;
+
+#if UNITY_2020_2_OR_NEWER
+#nullable enable
+#endif
+
+public class SimpleAUTDController : MonoBehaviour
+{
+    private Controller<AUTD3Sharp.Link.SOEM>? _autd = null;
+    public GameObject? Target = null;
+
+    private Vector3 _oldPosition;
+
+    private static bool _isPlaying = true;
+
+    private void Awake()
+    {
+        try
+        {
+            _autd = new ControllerBuilder()
+                .AddDevice(new AUTD3(gameObject.transform.position).WithRotation(gameObject.transform.rotation))
+                .Open(SOEM.Builder()
+                    .WithErrHandler((slave, status, msg) =>
+                    {
+                        switch (status)
+                        {
+                            case Status.Error:
+                                Debug.LogError($"Error [{slave}]: {msg}");
+                                break;
+                            case Status.Lost:
+                                Debug.LogError($"Lost [{slave}]: {msg}");
+                                // You can also wait for the link to recover, without exiting
+#if UNITY_EDITOR
+                                UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+                                UnityEngine.Application.Quit();
+#endif
+                                break;
+                            case Status.StateChanged:
+                                Debug.LogError($"StateChanged [{slave}]: {msg}");
+                                break;
+                        };
+                    }));
+        }
+        catch (Exception)
+        {
+            Debug.LogError("Failed to open AUTD3 controller!");
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+#endif
+        }
+
+        _autd!.Send(new AUTD3Sharp.Modulation.Sine(150)); // 150 Hz
+
+        if (Target == null) return;
+        _autd!.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
+        _oldPosition = Target.transform.position;
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (!_isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+            return;
+        }
+#endif
+        if (_autd == null) return;
+
+        if (Target == null || Target.transform.position == _oldPosition) return;
+        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
+        _oldPosition = Target.transform.position;
+    }
+
+    private void OnApplicationQuit()
+    {
+        _autd?.Dispose();
+    }
+}
+
+#if UNITY_2020_2_OR_NEWER
+#nullable restore
+#endif
