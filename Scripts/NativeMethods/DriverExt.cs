a446fae9375cdbb66400b8b09e9e73bb04805e05
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-22 13:09:56 +0900

    bump to v24.1.0

---
+++ Scripts/NativeMethods/DriverExt.cs
@@ -0,0 +1,230 @@
+using System;
+
+namespace AUTD3Sharp
+{
+    public enum GainSTMMode : byte
+    {
+        PhaseIntensityFull = 0,
+        PhaseFull = 1,
+        PhaseHalf = 2
+    }
+
+    public enum TimerStrategy : byte
+    {
+        Sleep = 0,
+        BusyWait = 1,
+        NativeTimer = 2
+    }
+
+    public enum SyncMode : byte
+    {
+        FreeRun = 0,
+        DC = 1
+    }
+
+    public enum Segment : byte
+    {
+        S0 = 0,
+        S1 = 1
+    }
+
+    public enum Status : byte
+    {
+        Error = 0,
+        StateChanged = 1,
+        Lost = 2
+    }
+
+    namespace NativeMethods
+    {
+        public static class StatusExt
+        {
+            public static AUTD3Sharp.Status Into(this Status mode)
+            {
+                return mode switch
+                {
+                    Status.Error => AUTD3Sharp.Status.Error,
+                    Status.StateChanged => AUTD3Sharp.Status.StateChanged,
+                    Status.Lost => AUTD3Sharp.Status.Lost,
+                    _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+                };
+            }
+        }
+
+        public static class SyncModeExt
+        {
+            public static SyncMode Into(this SyncMode mode)
+            {
+                return mode switch
+                {
+                    SyncMode.FreeRun => SyncMode.FreeRun,
+                    SyncMode.DC => SyncMode.DC,
+                    _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+                };
+            }
+        }
+
+        public static class TimerStrategyExt
+        {
+            public static TimerStrategy Into(this AUTD3Sharp.TimerStrategy strategy)
+            {
+                return strategy switch
+                {
+                    AUTD3Sharp.TimerStrategy.Sleep => TimerStrategy.Sleep,
+                    AUTD3Sharp.TimerStrategy.BusyWait => TimerStrategy.BusyWait,
+                    AUTD3Sharp.TimerStrategy.NativeTimer => TimerStrategy.NativeTimer,
+                    _ => throw new ArgumentOutOfRangeException(nameof(strategy), strategy, null)
+                };
+            }
+        }
+
+        public static partial class NativeMethodsBase
+        {
+            public const int AUTD3_ERR = -1;
+        }
+
+        public static class ResultExtensions
+        {
+            public static int Validate(this ResultI32 res)
+            {
+                if (res.result != NativeMethodsBase.AUTD3_ERR) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static ControllerPtr Validate(this ResultController res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static LinkSimulatorBuilderPtr Validate(this ResultLinkSimulatorBuilder res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static LinkRemoteSOEMBuilderPtr Validate(this ResultLinkRemoteSOEMBuilder res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static LinkRemoteTwinCATBuilderPtr Validate(this ResultLinkRemoteTwinCATBuilder res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static ModulationPtr Validate(this ResultModulation res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static GainCalcDrivesMapPtr Validate(this ResultGainCalcDrivesMap res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static FirmwareVersionListPtr Validate(this ResultFirmwareVersionList res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static DatagramPtr Validate(this ResultDatagram res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static BackendPtr Validate(this ResultBackend res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+            public static PlotConfigPtr Validate(this ResultPlotConfig res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+            public static PyPlotConfigPtr Validate(this ResultPyPlotConfig res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+
+            public static ModulationCalcPtr Validate(this ResultModulationCalc res)
+            {
+                if (res.result.Item1 != IntPtr.Zero) return res.result;
+                var err = new byte[res.err_len];
+                unsafe
+                {
+                    fixed (byte* p = &err[0]) NativeMethodsBase.AUTDGetErr(res.err, p);
+                }
+                throw new AUTDException(err);
+            }
+        }
+    }
+}
