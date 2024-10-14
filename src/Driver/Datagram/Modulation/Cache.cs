a8f42aa0ae7f67a6d44869e11bb3343b3406a1a8
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-14 13:30:23 +0900

    bump to v28.0.1

--- src/Driver/Datagram/Modulation/Cache.cs
+++ src/Driver/Datagram/Modulation/Cache.cs
@@ -4,59 +4,57 @@ using System.Diagnostics.CodeAnalysis;
 using AUTD3Sharp.Derive;
 using AUTD3Sharp.NativeMethods;
 
+#if UNITY_2020_2_OR_NEWER
+#nullable enable
+#endif
+
 namespace AUTD3Sharp.Driver.Datagram.Modulation
 {
     [Modulation(ConfigNoChange = true)]
-    public sealed partial class Cache<TM> : IEnumerable<byte>
+    public sealed partial class Cache<TM> : IDisposable
     where TM : IModulation
     {
         private readonly TM _m;
-        private byte[] _cache;
-        private AUTD3Sharp.NativeMethods.SamplingConfig? _samplingConfig;
+        private ModulationPtr? _ptr;
+        private bool _isDisposed;
 
         public Cache(TM m)
         {
             _m = m;
-            _cache = Array.Empty<byte>();
-            _loopBehavior = m.LoopBehavior;
+            _ptr = null;
+            _isDisposed = false;
         }
 
-        public ReadOnlySpan<byte> Init()
+        ~Cache()
         {
-            if (_cache.Length != 0) return Buffer;
-            var ptr = NativeMethodsBase.AUTDModulationCalc(_m.ModulationPtr());
-            var res = ptr.Validate();
-            _samplingConfig = ptr.config;
-            _cache = new byte[NativeMethodsBase.AUTDModulationCalcGetSize(res)];
-            unsafe
+            Dispose();
+
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed) return;
+
+            if (_ptr.HasValue)
             {
-                fixed (byte* pBuf = &_cache[0])
-                    NativeMethodsBase.AUTDModulationCalcGetResult(res, pBuf);
+                NativeMethodsBase.AUTDModulationCacheFree(_ptr.Value);
+                _ptr = null;
             }
-            NativeMethodsBase.AUTDModulationCalcFreeResult(res);
-            return Buffer;
+
+            _isDisposed = true;
+            GC.SuppressFinalize(this);
         }
 
         private ModulationPtr ModulationPtr()
         {
-            Init();
-            unsafe
-            {
-                fixed (byte* pBuf = &_cache[0])
-                    return NativeMethodsBase.AUTDModulationCustom(_samplingConfig!.Value, LoopBehavior, pBuf, (ushort)_cache.Length);
-            }
+            if (!_ptr.HasValue)
+                _ptr = NativeMethodsBase.AUTDModulationCache(_m
+                .ModulationPtr());
+            return NativeMethodsBase.AUTDModulationCacheClone(_ptr.Value);
         }
-
-        public byte this[int index] => _cache[index];
-
-        public ReadOnlySpan<byte> Buffer => new(_cache);
-
-        public IEnumerator<byte> GetEnumerator()
-        {
-            foreach (var e in _cache)
-                yield return e;
-        }
-
-        [ExcludeFromCodeCoverage] System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
+
+#if UNITY_2020_2_OR_NEWER
+#nullable restore
+#endif
