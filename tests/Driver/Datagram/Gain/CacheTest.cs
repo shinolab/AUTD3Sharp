d505b005f5e038310cdb988f73096a9d0e34a53f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-15 16:37:14 +0900

    fix cache tests

--- tests/Driver/Datagram/Gain/CacheTest.cs
+++ tests/Driver/Datagram/Gain/CacheTest.cs
@@ -17,18 +17,22 @@ public class CacheTest
     [Fact]
     public async Task Cache()
     {
-        var autd = await AUTDTest.CreateController();
-
-        await autd.SendAsync(new Uniform((new EmitIntensity(0x80), new Phase(0x90))).WithCache());
-
-        foreach (var dev in autd.Geometry)
         {
-            var (intensities, phases) = autd.Link.Drives(dev.Idx, Segment.S0, 0);
-            Assert.All(intensities, d => Assert.Equal(0x80, d));
-            Assert.All(phases, p => Assert.Equal(0x90, p));
+            var autd = await AUTDTest.CreateController();
+
+            await autd.SendAsync(new Uniform((new EmitIntensity(0x80), new Phase(0x90))).WithCache());
+
+            foreach (var dev in autd.Geometry)
+            {
+                var (intensities, phases) = autd.Link.Drives(dev.Idx, Segment.S0, 0);
+                Assert.All(intensities, d => Assert.Equal(0x80, d));
+                Assert.All(phases, p => Assert.Equal(0x90, p));
+            }
+
+            _ = new Uniform((new EmitIntensity(0x80), new Phase(0x90))).WithCache();
         }
 
-        _ = new Uniform((new EmitIntensity(0x80), new Phase(0x90))).WithCache();
+        GC.Collect();
     }
 
     [Fact]
--- tests/Driver/Datagram/Modulation/CacheTest.cs
+++ tests/Driver/Datagram/Modulation/CacheTest.cs
@@ -17,25 +17,29 @@ public class CacheTest
     [Fact]
     public async Task Cache()
     {
-        var autd1 = await Controller.Builder([new AUTD3(Vector3.Zero)]).OpenAsync(Audit.Builder());
-        var autd2 = await Controller.Builder([new AUTD3(Vector3.Zero)]).OpenAsync(Audit.Builder());
-
-        var m = new Sine(150 * Hz);
-        var mc = m.WithCache().WithLoopBehavior(LoopBehavior.Once);
-        Assert.Equal(LoopBehavior.Once, mc.LoopBehavior);
-        await autd1.SendAsync(m);
-        await autd2.SendAsync(mc);
-        foreach (var dev in autd2.Geometry)
         {
-            var modExpect = autd1.Link.Modulation(dev.Idx, Segment.S0);
-            var mod = autd2.Link.Modulation(dev.Idx, Segment.S0);
-            Assert.Equal(modExpect, mod);
-            Assert.Equal(LoopBehavior.Infinite, autd1.Link.ModulationLoopBehavior(dev.Idx, Segment.S0));
-            Assert.Equal(LoopBehavior.Once, autd2.Link.ModulationLoopBehavior(dev.Idx, Segment.S0));
-            Assert.Equal(autd1.Link.ModulationFreqDivision(dev.Idx, Segment.S0), autd2.Link.ModulationFreqDivision(dev.Idx, Segment.S0));
+            var autd1 = await Controller.Builder([new AUTD3(Vector3.Zero)]).OpenAsync(Audit.Builder());
+            var autd2 = await Controller.Builder([new AUTD3(Vector3.Zero)]).OpenAsync(Audit.Builder());
+
+            var m = new Sine(150 * Hz);
+            var mc = m.WithCache().WithLoopBehavior(LoopBehavior.Once);
+            Assert.Equal(LoopBehavior.Once, mc.LoopBehavior);
+            await autd1.SendAsync(m);
+            await autd2.SendAsync(mc);
+            foreach (var dev in autd2.Geometry)
+            {
+                var modExpect = autd1.Link.Modulation(dev.Idx, Segment.S0);
+                var mod = autd2.Link.Modulation(dev.Idx, Segment.S0);
+                Assert.Equal(modExpect, mod);
+                Assert.Equal(LoopBehavior.Infinite, autd1.Link.ModulationLoopBehavior(dev.Idx, Segment.S0));
+                Assert.Equal(LoopBehavior.Once, autd2.Link.ModulationLoopBehavior(dev.Idx, Segment.S0));
+                Assert.Equal(autd1.Link.ModulationFreqDivision(dev.Idx, Segment.S0), autd2.Link.ModulationFreqDivision(dev.Idx, Segment.S0));
+            }
+
+            _ = m.WithCache();
         }
 
-        _ = m.WithCache();
+        GC.Collect();
     }
 
     [Fact]
