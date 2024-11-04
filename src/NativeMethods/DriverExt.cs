237063f3723c80d0b944d3789dbeb7f8bcb66d2c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-04 09:50:41 +0900

    mv ffi into native methods

--- src/NativeMethods/DriverExt.cs
+++ src/NativeMethods/DriverExt.cs
@@ -190,5 +190,16 @@ namespace AUTD3Sharp
                 throw new AUTDException(err);
             }
         }
+
+        public static class Ffi
+        {
+            public static byte[] toNullTerminatedUtf8(string str)
+            {
+                var len = System.Text.Encoding.UTF8.GetByteCount(str);
+                var bytes = new byte[len + 1];
+                System.Text.Encoding.UTF8.GetBytes(str, 0, str.Length, bytes, bytes.GetLowerBound(0));
+                return bytes;
+            }
+        }
     }
 }
