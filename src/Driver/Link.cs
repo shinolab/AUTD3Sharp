8a9d112a0fc6ae018b8531aab6f203819651e67f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-04 16:09:37 +0900

    bump to v29.0.0-rc.4.1

--- src/Driver/Link.cs
+++ src/Driver/Link.cs
@@ -6,7 +6,7 @@ namespace AUTD3Sharp.Driver
     [ComVisible(false)]
     public interface ILinkBuilder<out T>
     {
-        internal LinkBuilderPtr Ptr();
-        internal T ResolveLink(RuntimePtr runtime, LinkPtr ptr);
+        public LinkBuilderPtr Ptr();
+        public T ResolveLink(RuntimePtr runtime, LinkPtr ptr);
     }
 }
