4c4ad8bc08b8bb447335d1a4ac1335f5d2bb3ea6
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-04 16:09:37 +0900

    bump to v29.0.0-rc.4.1

--- unity/Assets/Scripts/NativeMethods/DriverExt.cs
+++ unity/Assets/Scripts/NativeMethods/DriverExt.cs
@@ -9,30 +9,10 @@ namespace AUTD3Sharp
         PhaseHalf = 2
     }
 
-    public enum TimerStrategy : byte
-    {
-        SpinSleep = 0,
-        StdSleep = 1,
-        SpinWait = 2
-    }
-
     public enum SyncMode : byte
     {
         DC = 0,
         FreeRun = 1
-    }
-
-    namespace Link
-    {
-        public enum ProcessPriority : byte
-        {
-            Idle = 0,
-            BelowNormal = 1,
-            Normal = 2,
-            AboveNormal = 3,
-            High = 4,
-            Realtime = 5,
-        }
     }
 
     public enum Segment : byte
